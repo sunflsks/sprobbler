74caca7d7d6337de3d06b823d3b77800fd87a063
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2023-06-29 20:22:25 -0500

    base cli interface p2

--- serv/cli/cli.py
+++ serv/cli/cli.py
@@ -1,5 +1,10 @@
 import os
+from time import sleep
 
 
 def start_cli_interface() -> None:
-    print("CLI interface not implemented yet")
+    # sleep for a bit to let the flask server and other threads start up
+    sleep(2)
+    while True:
+        command = input("sprobbler> ")
+        print("silly goose")
