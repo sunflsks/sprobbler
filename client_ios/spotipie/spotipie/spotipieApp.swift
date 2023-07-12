511b3220cc5920cdd22144f13bbbc44f71d999a6
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2023-07-12 12:29:16 -0500

    app skeleton

---
+++ client_ios/spotipie/spotipie/spotipieApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotipieApp.swift
+//  spotipie
+//
+//  Created by Sudhip Nashi on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct spotipieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
