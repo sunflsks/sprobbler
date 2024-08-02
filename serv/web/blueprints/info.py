761259d141f2fb53dd3a42d2d7d048351e1531d0
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2024-08-02 15:12:14 -0500

    inject predicted genres into api returns

--- serv/web/blueprints/info.py
+++ serv/web/blueprints/info.py
@@ -1,6 +1,8 @@
+import db
 import json
 import requests
 from flask import Blueprint
+from peewee import PeeweeException
 from flask_dance.contrib.spotify import spotify  # type: ignore
 
 # since we use the json returned by spotify directly on the client, we can just have one wrapper
@@ -33,9 +35,21 @@ def root():
     return "available options are album, track, artist"
 
 
+# Inject predicted genre from DB
 @info_bp.route("/track/<string:track_id>")
 def track(track_id):
-    return get_value_from_spotify(f"/v1/tracks/{track_id}")
+    with db.database:
+        try:
+            predicted_genres = db.Track.get(db.Track.id == track_id.strip()).predicted_genre 
+        except db.DoesNotExist:
+            predicted_genres = None
+
+    response = get_value_from_spotify(f"/v1/tracks/{track_id}")
+    if not isinstance(response, dict):
+        return response
+
+    response["predicted_genres"] = predicted_genres if predicted_genres is not None else []
+    return response
 
 
 @info_bp.route("/album/<string:album_id>")
